1c7718467e2c916258181e014fd66e97c7b59a41
ber34 <ber34@o2.pl>
2018-02-01 11:18:56 +0100

    Update Class-Abstract-Polimorf.php

--- Class-Abstract-Polimorf.php
+++ Class-Abstract-Polimorf.php
@@ -1,3 +1,62 @@
 <?php
 
+abstract class Ksiegarnia{
+ 
+    public $tytul;
+	  public $autor;
+	  public $cena;
+ 
+     public abstract function setTytul($tytul);
+     public function getTytul(){
+	 
+	   return  $this->tytul;
+	 }
+	 
+	 public abstract function setAutor($autor);
+	 public function getAutor(){
+	 
+	 return  $this->autor;
+	 }
+	 
+	 public abstract function setCena($cena);
+	 public function getCena(){
+	 
+	 return  $this->cena;
+	 }
+ }
+ 
+ class Book extends Ksiegarnia{
+ 
+    public function setTytul($tytul){
+			 
+		  $this->tytul = $tytul;
+	    }
+		
+	public function setAutor($autor){
+			 
+		  $this->autor = $autor;
+	    }
 
+    public function setCena($cena){
+		 
+		 $this->cena = $cena;
+	    }	
+ }
+ 
+ class Komiks extends Ksiegarnia{
+ 
+    public function setTytul($tytul){
+			 
+		  $this->tytul = $tytul;
+	    }
+		
+	public function setAutor($autor){
+			 
+		  $this->autor = $autor;
+	    }
+
+    public function setCena($cena){
+		 
+		 $this->cena = $cena;
+	    }	
+ }
