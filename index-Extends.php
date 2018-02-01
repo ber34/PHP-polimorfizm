53bbb2e90e47e00bd4c4f8a724f006ce0e3e3d42
ber34 <ber34@o2.pl>
2018-02-01 11:17:27 +0100

    Update index-Extends.php

--- index-Extends.php
+++ index-Extends.php
@@ -1,4 +1,17 @@
 <?php
 
-
+  ####### Book ########
+ 
+ $tytulBook = "Book Manager Wydobycia";
+ $autorBook = "Adam Ber Book";
+ 
+   $book1 = new Book();
+  
+ $book1->setTytul($tytulBook);
+ // Tytul
+ echo $book1->getTytul()."<br>";
+ 
+  $book1->setAutor($autorBook);
+ // Autor
+ echo $book1->getAutor()."<br>";
 
