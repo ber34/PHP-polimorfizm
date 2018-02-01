57d0ec9b4f56fdaee97e38844e95dc9671991938
ber34 <ber34@o2.pl>
2018-02-01 11:16:12 +0100

    Update Class-Extends-Polimorf.php

--- Class-Extends-Polimorf.php
+++ Class-Extends-Polimorf.php
@@ -1,3 +1,42 @@
 <?php
 
-
+class Ksiegarnia{
+ 
+ public $tytul;
+ public $autor;
+ 
+ 
+   // Tytuł
+      public function getTytul(){
+			 
+		return  $this->tytul;
+	    }
+		
+	 public function setTytul($tytul){
+			 
+		  $this->tytul = $tytul;
+	    }
+	// Autor	
+	public function getAutor(){
+			 
+		return  $this->autor;
+	    }
+		
+	 public function setAutor($autor){
+			 
+		  $this->autor = $autor;
+	    }
+ }
+ 
+ class Book extends Ksiegarnia{
+ 
+    public function setTytul($tytul){
+			 
+		  $this->tytul = $tytul;
+	    }
+		
+	public function setAutor($autor){
+			 
+		  $this->autor = $autor;
+	    }
+ }
