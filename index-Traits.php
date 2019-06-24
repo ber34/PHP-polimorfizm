32fa553e226e2dc4ca3fab4c07bf8bfbc3a8b872
ber34 <ber34@o2.pl>
2019-06-24 17:55:26 +0200

    Update index-Traits.php

--- index-Traits.php
+++ index-Traits.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__) ."/Class-Abstract-Polimorf.php");
+require_once(dirname(__FILE__) ."/Class-Traits-Polimorf.php");
  ####### Book ########
 
  $tytulBook = "Book Manager Wydobycia";
