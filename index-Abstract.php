65818c7e9c4cccedccd3095b2b34f072fdd6521a
ber34 <ber34@o2.pl>
2019-06-24 17:22:41 +0200

    Update index-Abstract.php

--- index-Abstract.php
+++ index-Abstract.php
@@ -2,9 +2,11 @@
 
   ####### Book ########
  
- $tytulBook = "Book Manager Wydobycia";
- $autorBook = "Adam Ber Book";
- $cenaBook  = "10 zł";
+ $tytulBook    = "Book Manager Wydobycia";
+ $autorBook    = "Adam Ber Book";
+ $cenaBook     = "10 zł";
+ $isbn         = "12-234-435-1";
+
  
    $book1 = new Book();
   
@@ -19,12 +21,17 @@
   $book1->setCena($cenaBook);
    // Cena 
  echo $book1->getCena()."<br><br>";
+
+    $book1->setIsbn($isbn);
+   // Isbn 
+ echo $book1->getIsbn()."<br><br>";
  
   ####### Komiks ########
  
  $tytulKomiks = "Komiks Manager Wydobycia";
  $autorKomiks = "Adam Ber Komiks";
  $cenaKomiks  = "5 zł";
+ $seriesNumber = "10/99";
  
    $komiks1 = new Komiks();
   
@@ -39,3 +46,9 @@
   $komiks1->setCena($cenaKomiks);
    // Cena 
  echo $komiks1->getCena()."<br><br>";
+
+  $komiks1->setSeriesNumber($seriesNumber);
+   // Seria wydania numeru Series Number 
+ echo $komiks1->getSeriesNumber()."<br><br>";
+
+
