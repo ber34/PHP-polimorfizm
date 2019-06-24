3b16560cc74a07d18a4adc1d0e1a651a84770478
ber34 <ber34@o2.pl>
2019-06-24 17:17:16 +0200

    Update Class-Abstract-Polimorf.php

--- Class-Abstract-Polimorf.php
+++ Class-Abstract-Polimorf.php
@@ -2,32 +2,40 @@
 
 abstract class Ksiegarnia{
  
-    public $tytul;
+          public $tytul;
 	  public $autor;
 	  public $cena;
  
      public abstract function setTytul($tytul);
+	
      public function getTytul(){
 	 
 	   return  $this->tytul;
+	     
 	 }
 	 
 	 public abstract function setAutor($autor);
+	
 	 public function getAutor(){
 	 
 	 return  $this->autor;
+		 
 	 }
 	 
 	 public abstract function setCena($cena);
+	
 	 public function getCena(){
 	 
 	 return  $this->cena;
+		 
 	 }
  }
  
  class Book extends Ksiegarnia{
  
-    public function setTytul($tytul){
+	 private $isbn;
+	 
+        public function setTytul($tytul){
 			 
 		  $this->tytul = $tytul;
 	    }
@@ -37,15 +45,27 @@ abstract class Ksiegarnia{
 		  $this->autor = $autor;
 	    }
 
-    public function setCena($cena){
+        public function setCena($cena){
 		 
 		 $this->cena = $cena;
+	    }	
+	 
+	 public function setIsbn($isbn){
+			 
+		  $this->isbn = $isbn;
+	    }
+
+         public function getIsbn(){
+		 
+		 return $this->isbn;
 	    }	
  }
  
  class Komiks extends Ksiegarnia{
  
-    public function setTytul($tytul){
+	 private $seriesNumber; 
+	 
+        public function setTytul($tytul){
 			 
 		  $this->tytul = $tytul;
 	    }
@@ -55,8 +75,18 @@ abstract class Ksiegarnia{
 		  $this->autor = $autor;
 	    }
 
-    public function setCena($cena){
+        public function setCena($cena){
 		 
 		 $this->cena = $cena;
-	    }	
+	    }
+	 
+	 public function setSeriesNumber($seriesNumber){
+			 
+		  $this->seriesNumber = $seriesNumber;
+	    }
+
+          public function getSeriesNumber(){
+		 
+		 return $this->seriesNumber;
+	    }
  }
